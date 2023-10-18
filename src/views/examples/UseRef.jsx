0401ccc912a468a97b5fe835ca2c311df81194df
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-10-18 19:22:35 -0300

    change useRef component

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 import SectionTitle from '../../components/layout/SectionTitle'
+import { useRef } from 'react'
 
 const UseRef = (props) => {
+
+    const [value1, setValue1] = useState("")
+    const count = useRef(0)
+    count.current++
+
     return (
         <div className="UseRef">
             <PageTitle
@@ -13,7 +19,16 @@ const UseRef = (props) => {
 
             <SectionTitle title="Exercicio 01"></SectionTitle>
             <div className="center">
-                <input type="text" className="input" />
+                <span className="text">Valor: {value1} - [ {count.current} ]</span>
+                <input type="text" className="input"
+                    value={value1}
+                    onChange={e => setValue1(e.target.value)}
+                />
+            </div>
+
+            <SectionTitle title="Exercicio 02"></SectionTitle>
+            <div className="center">
+                
             </div>
 
 
