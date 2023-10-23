d643c3f88c2da7812560bfa26ac8a86163a440f7
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-10-23 10:48:56 -0300

    CRiado componente Store

--- src/data/Store.jsx
+++ src/data/Store.jsx
@@ -1,2 +1,12 @@
 import React from "react";
 
+const Store = props => {
+
+    return (
+        <div>
+            Store
+        </div>
+    )
+}
+
+export default Store;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 import DataContext, { data } from '../data/DataContext'
+import Store from '../data/Store'
 
 const App = props => {
 
@@ -12,12 +13,14 @@ const App = props => {
 
     return (
         <div className="App">
-            <DataContext.Provider value={ {state, setState} }>
-                <Router>
-                    <Menu />
-                    <Content />
-                </Router>
-            </DataContext.Provider>
+            <Store>
+                <DataContext.Provider value={{ state, setState }}>
+                    <Router>
+                        <Menu />
+                        <Content />
+                    </Router>
+                </DataContext.Provider>
+            </Store>
         </div>
     )
 }
