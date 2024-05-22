3488b49d2bb1a3ab9ee79403e4c6d3151c9aa770
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-05-22 13:32:51 -0300

    iniciando user reducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const inicialEstado = {
+    other: '...',
+    number: 0
+}
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'add2':
+            return {...state, number: state.number + 2}
+            break;
+        case 'oth' :
+            return {}
+            break;    
+        default:
+            return state
+            break;
+    }
+}
+
 const UseReducer = (props) => {
     return (
         <div className="UseReducer">
