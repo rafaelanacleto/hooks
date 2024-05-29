9b0566487e29803358e1010b7b403c0be094b307
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-05-29 16:09:17 -0300

    Criando o proprio hook personalizado

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const useCounter = initialValue => {
+export const useCounter = (initialValue = 100) => {
 
+    const [count, setCount] = useState(initialValue)
+
+    function inc() {
+        setCount(count + 1)
+    }
+
+    function dec() {
+        setCount(count -1)
+    }
+
+    return [count, inc, dec]
     
 }
--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import { useCounter } from '../../hooks/useCounter'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+
+    const [count, inc, dec] = useCounter(500)
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <SectionTitle title="Exercicio 01"></SectionTitle>
+            <div className="center">
+                <span className="text">{count}</span>
+            </div>
+            <div className="center">
+                <button className="btn" onClick={() => inc()}>Inc</button>
+                <button className="btn" onClick={() => dec()}>Dec</button>
+            </div>
         </div>
     )
 }
